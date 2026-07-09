e5d2a27d94feb344a789a8897d4764b3d77f5bf1
Lucca Juliatti <luccajuliatti@Luccas-MacBook-Air.local>
2026-07-09 19:00:26 -0300

    chore: remove dead redirect routes (#356, #358, #359)

--- src/pages/VaziosRedirect.tsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, useLocation } from 'react-router-dom'
-
-export function VaziosRedirect() {
-  const location = useLocation()
-  return <Navigate to={`/embarquevazios${location.search}`} replace />
-}
